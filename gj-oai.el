bd6b04d9d0353e115cc1cf649c282196735ea2d9
Grant Jenks <grant.jenks@gmail.com>
2023-04-13 00:30:01 -0700

    Use process instead of http request

--- gj-oai.el
+++ gj-oai.el
@@ -1,33 +1,22 @@
-(require 'request)
-
-(defun read-api-key-from-dotenv (file)
-  "Read the OpenAI API key from the dotenv FILE."
-  (with-temp-buffer
-    (insert-file-contents file)
-    (goto-char (point-min))
-    (when (re-search-forward "^export OPENAI_API_KEY=\\(.*\\)$" nil t)
-      (match-string 1))))
-
-(defun chat-completions-gpt-4 (message)
-  "Query the OpenAI Chat Completions endpoint using gpt-4 model with MESSAGE."
-  (interactive "sEnter your message: ")
-  (let* ((dotenv-file (expand-file-name ".env" "~"))
-         (api-key (read-api-key-from-dotenv dotenv-file))
-         (url "https://api.openai.com/v1/chat/completions")
-         (data (json-encode-alist `(("model" . "gpt-4")
-                                     ("messages" . [(( "role" . "user") ("content" . ,message))])))))
-    (if api-key
-        (request
-         url
-         :type "POST"
-         :headers `(("Content-Type" . "application/json")
-                    ("Authorization" . ,(concat "Bearer " api-key)))
-         :data data
-         :parser 'json-read
-         :success (cl-function
-                   (lambda (&key data &allow-other-keys)
-                     (let ((response (aref (alist-get 'choices data) 0)))
-                       (message "Response: %s" (alist-get 'content (alist-get 'message response)))))))
-      (message "Error: API key not found in %s" dotenv-file))))
-
-(global-set-key (kbd "C-c g") 'chat-completions-gpt-4)
+(defun chat-completion (prefix suffix query)
+  "Send a query to a Chat Completion process and display the output in a new buffer."
+  (interactive
+   (if (region-active-p)
+       (list
+        (read-string "Prefix: ")
+        (read-string "Suffix: ")
+        (buffer-substring-no-properties (region-beginning) (region-end)))
+     (list
+      nil
+      nil
+      (read-string "Query: "))))
+  (compile (format "/bin/bash -ic \"echo '%s' | %s %s %s\""
+                   (if (region-active-p)
+                       (concat prefix "\n```\n" query "\n```\n" suffix)
+                     query)
+                   "/Users/grantjenks/repos/dotemacs/oai"
+                   "-"
+                   "-")
+           t)
+  (setq-local compilation-read-command nil)
+  (pop-to-buffer compilation-last-buffer))
